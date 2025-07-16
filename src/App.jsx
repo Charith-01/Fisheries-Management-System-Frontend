cd53f798866b3c8c58c7fa81823cacbfff7e2abb
Charith Lakshan <it23841086@my.sliit.lk>
2025-07-16 19:01:43 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
 import Header from './components/header'
 import ProductCard from './components/product.card'
@@ -5,10 +6,16 @@ import AdminDashboard from './pages/adminDashboard'
 import LoginPage from './pages/loginPage'
 
 function App() {
-
   return (
-    // <LoginPage/>
-    <AdminDashboard/>
+    <BrowserRouter>
+      <Routes path="/*">
+        <Route path="/admin/*" element={<AdminDashboard/>}/>
+        <Route path="/login" element={<LoginPage/>}/>
+        <Route path="/" element={<h1>Home</h1>}/>
+        <Route path="/*" element={<h1>404 Not Found</h1>}/>
+      </Routes>
+    </BrowserRouter>
+
   )
 }
 
