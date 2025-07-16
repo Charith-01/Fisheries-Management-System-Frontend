47b793da0a356e0282d23ed9253a263110ae2e44
Charith Lakshan <it23841086@my.sliit.lk>
2025-07-16 13:09:29 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import './App.css'
+import Header from './components/header'
+import ProductCard from './components/product.card'
+import AdminDashboard from './pages/adminDashboard'
+import LoginPage from './pages/loginPage'
 
 function App() {
 
   return (
-    <>
-      
-    </>
+    // <LoginPage/>
+    <AdminDashboard/>
   )
 }
 
