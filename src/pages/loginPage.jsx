0e6845e02f9a166170e668c236530cec89895283
Charith Lakshan <it23841086@my.sliit.lk>
2025-07-16 18:57:52 +0530

    add userState hook

--- src/pages/loginPage.jsx
+++ src/pages/loginPage.jsx
@@ -1,4 +1,16 @@
+import { useState } from "react"
+
 export default function LoginPage(){
+
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+
+    function handleLogin(){
+        console.log("Email: ", email)
+        console.log("Password: ", password)
+        console.log("Login button clicked")
+    }
+
     return(
         <div className="w-full h-screen bg-[url(/login-bg.jpg)] bg-cover bg-center bg-no-repeat flex">
             <div className="w-[50%] h-full">
@@ -6,9 +18,17 @@ export default function LoginPage(){
             </div>
             <div className="w-[50%] h-full flex justify-center items-center">
                 <div className="w-[450px] h-[600px] backdrop-blur-xl shadow-2xl rounded-xl flex justify-center items-center flex-col">
-                        <input className="w-[400px] h-[50px] border border-white rounded-2xl text-center m-[5px]" type="email" placeholder="Email"/>
-                        <input className="w-[400px] h-[50px] border border-white rounded-2xl text-center m-[5px]" type="password" placeholder="Password"/>
-                        <button className="w-[380px] h-[50px] bg-blue-500 rounded-xl text-white cursor-pointer">Login</button>
+                        <input onChange={
+                            (e)=>{
+                                setEmail(e.target.value)
+                            }
+                        } className="w-[400px] h-[50px] border border-white rounded-2xl text-center m-[5px]" type="email" placeholder="Email"/>
+                        <input onChange={
+                            (e)=>{
+                                setPassword(e.target.value)
+                            }
+                        } className="w-[400px] h-[50px] border border-white rounded-2xl text-center m-[5px]" type="password" placeholder="Password"/>
+                        <button onClick={handleLogin} className="w-[380px] h-[50px] bg-blue-500 rounded-xl text-white cursor-pointer">Login</button>
                 </div>
             </div>
         </div>
