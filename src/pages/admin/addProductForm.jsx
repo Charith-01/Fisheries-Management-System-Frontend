7e46799ea842c79fd1079d8eaec1afb6c59e1273
Charith Lakshan <it23841086@my.sliit.lk>
2025-07-17 19:22:12 +0530

    Update

--- src/pages/admin/addProductForm.jsx
+++ src/pages/admin/addProductForm.jsx
@@ -1,7 +1,24 @@
+import { Link } from "react-router-dom";
+
 export default function AddProductForm(){
     return(
-        <div>
-            
+        <div className="w-full h-full flex justify-center items-center rounded-lg">
+            <div className="w-[500px] h-[700px] bg-white rounded-lg shadow-2xl flex flex-col items-center">
+                <h1 className="text-3xl font-bold text-gray-700 m-[10px]">Add Product</h1>
+                <input className="w-[400px] h-[50px] border border-gray-500 rounded-2xl text-center m-[5px]" type="text" placeholder="Product ID"/>
+                <input className="w-[400px] h-[50px] border border-gray-500 rounded-2xl text-center m-[5px]" type="text" placeholder="Product Name"/>
+                <input className="w-[400px] h-[50px] border border-gray-500 rounded-2xl text-center m-[5px]" type="text" placeholder="Alternative Names"/>
+                <input className="w-[400px] h-[50px] border border-gray-500 rounded-2xl text-center m-[5px]" type="text" placeholder="Price"/>
+                <input className="w-[400px] h-[50px] border border-gray-500 rounded-2xl text-center m-[5px]" type="text" placeholder="Labeled Price"/>
+                <input className="w-[400px] h-[50px] border border-gray-500 rounded-2xl text-center m-[5px]" type="text" placeholder="Stock"/>
+                <input className="w-[400px] h-[50px] border border-gray-500 rounded-2xl text-center m-[5px]" type="text" placeholder="Product Category"/>
+                <input className="w-[400px] h-[50px] border border-gray-500 rounded-2xl text-center m-[5px]" type="text" placeholder="Unit"/>
+                <textarea className="w-[400px] h-[50px] border border-gray-500 rounded-2xl text-center m-[5px]" placeholder="Description"/>
+                <div className="w-[400px flex justify-between items-center">
+                    <Link to={"/admin/products"} className="bg-red-500 rounded-xl text-white cursor-pointer hover:bg-red-700 p-4 w-[200px] m-3 text-center">Cancel</Link>
+                    <Link to={"/admin/products"} className="bg-green-500 rounded-xl text-white cursor-pointer hover:bg-green-700 p-4 w-[200px] m-3 text-center">Cancel</Link>
+                </div>
+            </div>
         </div>
     )
 }
