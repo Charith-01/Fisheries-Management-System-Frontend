0d822c185e8d137bee258593a2d5148ab3dc741b
Charith Lakshan <it23841086@my.sliit.lk>
2025-07-21 20:58:37 +0530

    Create deleteProduct function

--- src/pages/admin/products.jsx
+++ src/pages/admin/products.jsx
@@ -2,6 +2,9 @@ import axios from "axios"
 import { useEffect, useState } from "react"
 import { FaPlus } from "react-icons/fa";
 import { Link } from "react-router-dom";
+import { FaRegTrashAlt } from "react-icons/fa";
+import { FaRegEdit } from "react-icons/fa";
+import toast from "react-hot-toast";
 
 export default function AdminProductsPage(){
 
@@ -19,6 +22,28 @@ export default function AdminProductsPage(){
         []
     )
 
+    async function deleteProduct(id){
+        const token = localStorage.getItem("token")
+        
+        if(token == null){
+            toast.error("Please login to delete a product")
+            return
+        }
+        try{
+            await axios.delete(import.meta.env.VITE_BACKEND_URL+"/api/product/"+id, {
+                headers:{
+                    Authorization: "Bearer "+token
+                }
+            })
+            toast.success("Product deleted successfully")
+
+        }catch(err){
+            console.log(err)
+            toast.error("Product deleting failed")
+            return;
+        }
+    }
+
     return(
         <div className="w-full h-full rounded-lg relative">
             <Link to={"/admin/addProduct"} className="absolute right-6 bottom-6 text-white bg-gray-700 p-[12px] rounded-full text-3xl cursor-pointer hover:bg-gray-400 hover:text-gray-700">
@@ -34,16 +59,15 @@ export default function AdminProductsPage(){
                         <th className="p-2">Stock</th>
                         <th className="p-2">Category</th>
                         <th className="p-2">Unit</th>
+                        <th className="p-2">Action</th>
                     </tr>
                 </thead>
                 <tbody>
                 {
                     products.map(
                         (product, index)=>{
-                            console.log(product.name)
-
                             return(
-                                <tr key={index} className="border-b-2 border-gray-300 text-center cursor-pointer hover:bg-gray-800 hover:text-white">
+                                <tr key={index} className="border-b-2 border-gray-300 text-center hover:bg-gray-100">
                                     <td className="p-2">{product.productId}</td>
                                     <td className="p-2">{product.name}</td>
                                     <td className="p-2">{product.price}</td>
@@ -51,6 +75,12 @@ export default function AdminProductsPage(){
                                     <td className="p-2">{product.stock}</td>
                                     <td className="p-2">{product.category}</td>
                                     <td className="p-2">{product.unit}</td>
+                                    <td className="p-2">
+                                        <div className="w-full h-full flex  justify-center">
+                                            <FaRegTrashAlt onClick={()=>{deleteProduct(product.productId)}} className="text-3xl m-[10px] hover:text-red-500 cursor-pointer" />
+                                            <FaRegEdit className="text-3xl m-[10px] hover:text-blue-500 cursor-pointer" />
+                                        </div>
+                                    </td>
                                 </tr>
                             )
                         }
