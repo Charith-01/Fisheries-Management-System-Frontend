1e6a4a1ec999c1d9f5051631e95daf1c1575361f
Charith Lakshan <it23841086@my.sliit.lk>
2025-07-22 17:14:45 +0530

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import ProductCard from './components/product.card'
 import AdminDashboard from './pages/adminDashboard'
 import LoginPage from './pages/loginPage'
 import { Toaster } from 'react-hot-toast'
+import RegisterPage from './pages/client/registerPage'
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
       <Routes path="/*">
         <Route path="/admin/*" element={<AdminDashboard/>}/>
         <Route path="/login" element={<LoginPage/>}/>
+        <Route path="/register" element={<RegisterPage/>}/>
         <Route path="/" element={<h1>Home</h1>}/>
         <Route path="/*" element={<h1>404 Not Found</h1>}/>
       </Routes>
