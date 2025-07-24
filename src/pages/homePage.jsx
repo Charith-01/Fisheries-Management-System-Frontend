31b552fcec8fb6c32a56a792a41cf957a6277de7
Charith Lakshan <it23841086@my.sliit.lk>
2025-07-24 18:57:33 +0530

    Create homePage.jsx

---
+++ src/pages/homePage.jsx
@@ -0,0 +1,12 @@
+import Header from "../components/header";
+
+export default function HomePage(){
+    return(
+        <div className="w-full h-screen max-h-screen">
+            <Header/>
+            <div className="w-full h-[calc(100vh-75px)]">
+
+            </div>
+        </div>
+    )
+}
