f2662d7084f847634463d750feba1260407da222
Charith Lakshan <it23841086@my.sliit.lk>
2025-07-24 23:42:36 +0530

    Update

--- src/pages/client/registerPage.jsx
+++ src/pages/client/registerPage.jsx
@@ -7,9 +7,9 @@ export default function RegisterPage() {
     const [firstName, setFirstName] = useState("");
     const [lastName, setLastName] = useState("");
     const [email, setEmail] = useState("");
+    const [phone, setPhone] = useState("");
     const [password, setPassword] = useState("");
     const [confirmPassword, setConfirmPassword] = useState("");
-    const [phone, setPhone] = useState("");
     const [loading, setLoading] = useState(false);
 
     const navigate = useNavigate();
@@ -26,8 +26,8 @@ export default function RegisterPage() {
                 firstName,
                 lastName,
                 email,
-                password,
                 phone,
+                password,
             });
 
             toast.success("Registration successful");
@@ -41,60 +41,67 @@ export default function RegisterPage() {
     }
 
     return (
-        <div className="w-full h-screen bg-[url(/login-bg.jpg)] bg-cover bg-center bg-no-repeat flex">
-            <div className="w-[50%] h-full"></div>
-            <div className="w-[50%] h-full flex justify-center items-center">
-                <div className="w-[450px] min-h-[650px] backdrop-blur-xl shadow-2xl rounded-xl flex justify-center items-center flex-col p-5 space-y-3">
+        <div className="w-full h-screen flex">
+            {/* Left Side - White Box with Form */}
+            <div className="w-[40%] h-full bg-white flex justify-center items-center">
+                <div className="w-[450px] min-h-[750px] flex justify-center items-center flex-col">
+                    <div className="w-[140px] h-[90px] bg-[url(/logo.jpg)] bg-cover bg-center bg-no-repeat mb-8"></div>
+                    <h1 className="font-semibold text-gray-700 text-xl">Create Your Account</h1>
+                    <label className="text-gray-700 mb-3">Register with your details</label>
+
                     <input
-                        type="text"
-                        placeholder="First Name"
                         onChange={(e) => setFirstName(e.target.value)}
-                        className="w-[400px] h-[45px] border border-white rounded-2xl text-center"
-                    />
-                    <input
                         type="text"
-                        placeholder="Last Name"
+                        placeholder="First Name*"
+                        className="w-[400px] h-[50px] border border-blue-500 focus:ring-1 focus:ring-blue-700 outline-none transition-all text-center m-[10px]"
+                    />
+                    <input
                         onChange={(e) => setLastName(e.target.value)}
-                        className="w-[400px] h-[45px] border border-white rounded-2xl text-center"
+                        type="text"
+                        placeholder="Last Name*"
+                        className="w-[400px] h-[50px] border border-blue-500 focus:ring-1 focus:ring-blue-700 outline-none transition-all text-center m-[10px]"
                     />
                     <input
-                        type="email"
-                        placeholder="Email"
                         onChange={(e) => setEmail(e.target.value)}
-                        className="w-[400px] h-[45px] border border-white rounded-2xl text-center"
+                        type="email"
+                        placeholder="Email address*"
+                        className="w-[400px] h-[50px] border border-blue-500 focus:ring-1 focus:ring-blue-700 outline-none transition-all text-center m-[10px]"
                     />
                     <input
-                        type="tel"
-                        placeholder="Phone"
                         onChange={(e) => setPhone(e.target.value)}
-                        className="w-[400px] h-[45px] border border-white rounded-2xl text-center"
+                        type="tel"
+                        placeholder="Phone number*"
+                        className="w-[400px] h-[50px] border border-blue-500 focus:ring-1 focus:ring-blue-700 outline-none transition-all text-center m-[10px]"
                     />
                     <input
-                        type="password"
-                        placeholder="Password"
                         onChange={(e) => setPassword(e.target.value)}
-                        className="w-[400px] h-[45px] border border-white rounded-2xl text-center"
+                        type="password"
+                        placeholder="Password*"
+                        className="w-[400px] h-[50px] border border-blue-500 focus:ring-1 focus:ring-blue-700 outline-none transition-all text-center m-[10px]"
                     />
                     <input
-                        type="password"
-                        placeholder="Confirm Password"
                         onChange={(e) => setConfirmPassword(e.target.value)}
-                        className="w-[400px] h-[45px] border border-white rounded-2xl text-center"
+                        type="password"
+                        placeholder="Confirm Password*"
+                        className="w-[400px] h-[50px] border border-blue-500 focus:ring-1 focus:ring-blue-700 outline-none transition-all text-center m-[10px]"
                     />
                     <button
                         onClick={handleRegister}
-                        className="w-[400px] h-[50px] bg-blue-500 rounded-xl text-white cursor-pointer"
+                        className="w-[400px] h-[50px] bg-blue-500 rounded-lg text-white cursor-pointer m-[15px] hover:bg-blue-600"
                     >
                         {loading ? "Registering..." : "Register"}
                     </button>
-                    <p className="text-gray-300">
+                    <p className="text-gray-700 m-[5px]">
                         Already have an account?&nbsp;
-                        <span className="text-blue-700 cursor-pointer hover:text-blue-900">
+                        <span className="text-blue-500 cursor-pointer hover:text-blue-700 font-medium">
                             <Link to="/login">Login</Link>
                         </span>
                     </p>
                 </div>
             </div>
+
+            {/* Right Side - Background Image */}
+            <div className="w-[60%] h-full bg-[url(/login-bg.jpg)] bg-cover bg-center bg-no-repeat"></div>
         </div>
     );
 }
