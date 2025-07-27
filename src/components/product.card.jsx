93951465f5c18aa7e0a42e851cd9fa3e28fb813a
Charith Lakshan <it23841086@my.sliit.lk>
2025-07-27 21:07:12 +0530

    Update

--- src/components/product.card.jsx
+++ src/components/product.card.jsx
@@ -5,8 +5,13 @@ export default function ProductCard(props){
     const product = props.product;
 
     return(
-        <Link className="w-[250px] h-[350px] bg-amber-500 border m-4">
-
+        <Link to={"/overview/"+product.productId} className="w-[250px] h-[350px] m-4 hover:shadow-xl shadow">
+            <img className="w-full h-[220px] object-cover" src={product.images[0]}/>
+            <div className="h-[120px] w-full flex justify-center px-4 flex-col">
+                <p className="text-gray-400">{product.productId}</p>
+                <p className="text-lg font-bold">{product.name}</p>
+                <p className="text-lg text-blue-600 font-semibold">{product.price.toFixed(2)} <span className="line-through text-gray-500 text-sm">{product.price<product.labeledPrice&&product.labeledPrice.toFixed(2)}</span></p>
+            </div>
         </Link>
     )
 }
