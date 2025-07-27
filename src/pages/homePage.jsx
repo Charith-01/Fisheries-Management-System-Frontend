a7dd467c86bb815e7f37ff7290f306741aae3530
Charith Lakshan <it23841086@my.sliit.lk>
2025-07-27 13:45:58 +0530

    Update

--- src/pages/homePage.jsx
+++ src/pages/homePage.jsx
@@ -1,11 +1,19 @@
+import { Route, Routes } from "react-router-dom";
 import Header from "../components/header";
+import ProductsPage from "./client/productsPage";
 
 export default function HomePage(){
     return(
         <div className="w-full h-screen max-h-screen">
             <Header/>
-            <div className="w-full h-[calc(100vh-75px)]">
-
+            <div className="w-full h-[calc(100vh-70px)]">
+                <Routes path="/*">
+                    <Route path="/" element={<h1>Home page</h1>}/>
+                    <Route path="/products" element={<ProductsPage/>} />
+                    <Route path="/contact" element={<h1>Contact page</h1>} />
+                    <Route path="/reviews" element={<h1>Reviews page</h1>} />
+                    <Route path="/*" element={<h1>404 not found</h1>}/>
+                </Routes>
             </div>
         </div>
     )
