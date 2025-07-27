6b399411e4881d43ccf6ed57a451b9d52d12ade4
Charith Lakshan <it23841086@my.sliit.lk>
2025-07-27 14:09:00 +0530

    Update

--- src/pages/client/productsPage.jsx
+++ src/pages/client/productsPage.jsx
@@ -1,10 +1,24 @@
+import axios from "axios";
+import { useEffect, useState } from "react"
+
 export default function ProductsPage(){
+
+    const [productList, setProductList] = useState([]);
+    const [loaded, setLoaded] = useState(false);
+
+    useEffect(
+        ()=>{
+            if(!loaded){
+                axios.get(import.meta.env.VITE_BACKEND_URL+"/api/product").then(
+                    (res)=>{
+                        setProductList(res.data);
+                        setLoaded(true)
+                    }
+                )
+            }
+        },[loaded]
+    )
 
     
 
-    return(
-        <div className="h-full w-full">
-
-        </div>
-    )
 }
